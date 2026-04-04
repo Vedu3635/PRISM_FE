391045998ab3a704c7003291fcc6bc858c995623
Vedu3635 <paghdarvedantvp39@gmail.com>
2026-04-04 20:10:39 +0530

    Fix case-sensitive import for LandingPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import PublicLayout from './layouts/PublicLayout';
 import AuthenticatedLayout from './layouts/AuthenticatedLayout';
-import LandingPage from './pages/landingpage';
+import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Onboarding from './pages/Onboarding';

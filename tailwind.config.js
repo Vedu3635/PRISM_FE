41c049b43d61cf1a2785a5ef7a1b822dc474f760
Vedu3635 <paghdarvedantvp39@gmail.com>
2026-04-11 18:42:17 +0530

    removed conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
-  darkMode: ["class"],
-=======
   darkMode: "class",
->>>>>>> main
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -12,50 +8,6 @@ export default {
   theme: {
     extend: {
       colors: {
-<<<<<<< HEAD
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-    },
-  },
-  plugins: [require("tailwindcss-animate")],
-=======
         "on-tertiary-fixed": "#351000",
         "on-error": "#690005",
         "inverse-primary": "#494bd6",
@@ -110,13 +62,12 @@ export default {
         "label": ["Inter"]
       },
       borderRadius: {
-        "DEFAULT": "0.25rem", 
-        "lg": "0.5rem", 
-        "xl": "0.75rem", 
+        "DEFAULT": "0.25rem",
+        "lg": "0.5rem",
+        "xl": "0.75rem",
         "full": "9999px"
       },
     },
   },
   plugins: [],
->>>>>>> main
 }
